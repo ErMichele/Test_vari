69a9eb75ed5671ba1295285d6dc3ed06024375f7
Michele Acciuffi <micheleacciuffi@gmail.com>
2024-11-13 10:13:12 +0100

    Add files via upload

---
+++ C/File/Testo/test1.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <windows.h>
+#include <string.h>
+
+
+
+int main (){
+    int m;
+    FILE *file = fopen ("test1.txt", "a");
+    if (file==NULL) {
+        printf ("Il file non è stat trovato.\n");
+        
+    }
+    printf ("\nScrivi i numeri da mettere nel file: ");
+    scanf ("%d", &m);
+    fprintf (file, "%d\n", m);
+
+    fclose (file);
+}
